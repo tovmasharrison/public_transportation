859e937f219770028cd8575b60ceaa5c4e2accff
Tom from real python <tharrison748@gmail.com>
2023-03-12 22:05:16 +0400

    Update User model

--- user/models.py
+++ user/models.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+from django.conf import settings
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -6,10 +7,13 @@ from django.dispatch import receiver
 from helpers.image_paths import default_profile_image, upload_profile_image
 
 
+User = get_user_model()
+
+
 class Profile(models.Model):
     """ User Profile model """
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_of_birth = models.DateField(blank=True, null=True)
     image = models.ImageField(upload_to=upload_profile_image, default=default_profile_image)
 
